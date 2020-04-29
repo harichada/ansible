8c5795c4c902a391fcceb0ba29e8fe3bf38b3789
Harikishore Chada <harikishore.chada@dcss.ca.gov>
2020-04-29 11:48:38 -0700

    t pu

--- tomcat/ports.sh
+++ tomcat/ports.sh
@@ -16,7 +16,8 @@ do
     elif [ $i -eq 5 ]
     then
        https=$port
-       ansible-playbook -i ../inventory updateXML.yml --extra-vars "var_host=sytas env=$env http=$http https=$https ajp=$ajp shutdown=$shutdown" -K
+#       ansible-playbook -i ../inventory updateXML.yml --extra-vars "var_host=sytas env=$env http=$http https=$https ajp=$ajp shutdown=$shutdown" -K
+echo $i
        i=0
     fi
 i=$[$i+1]
